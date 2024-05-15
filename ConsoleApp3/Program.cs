89beb83fd0cedd24af1e674a7c264fac637626d6
Mileñum <mileprevi@gmail.com>
2024-05-14 22:50:10 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacroParcial2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int tarifaBase = 50;
+            int costoKm = 10;
+            int cargoExtra = 20;
+         
+
+            Console.WriteLine("Por favor, ingresa la distancia del viaje en kilómetros:");
+            double distancia = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la duración del viaje en minutos");
+            int tiempo = int.Parse(Console.ReadLine());
+            double costoViaje = tarifaBase + (distancia * costoKm);
+            if (tiempo > 30)
+            {
+                costoViaje += cargoExtra;
+            }
+            Console.WriteLine("El costo total del viaje es:" + costoViaje);
+            Console.Read();
+
+        }
+    }
+}
